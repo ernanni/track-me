9a355a54b0598f7d53eb8d182208bedca347e7c0
Fabio Ernanni <ernannicf@gmail.com>
2022-10-19 13:45:11 -0300

    finished track server

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,13 +1,16 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const trackRouter = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(express.json());
 app.use(authRoutes);
+app.use(trackRouter);
 
 const mongoUri = 'mongodb+srv://admin:punisher1974@cluster0.33a3y.mongodb.net/?retryWrites=true&w=majority';
 
